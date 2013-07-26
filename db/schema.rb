2e223d658d9181bc269ce563f9938bddc2dd29ee
Ginger Griffis <gingergriffis@hotmail.com>
2013-07-26 15:15:21 -0700

    added migration to add codefellow fields to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130726200036) do
+ActiveRecord::Schema.define(:version => 20130726214929) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(:version => 20130726200036) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
+    t.text     "street_address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.text     "introcution"
+    t.text     "desired_job_situation"
+    t.text     "desired_job_location"
+    t.text     "skills"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
