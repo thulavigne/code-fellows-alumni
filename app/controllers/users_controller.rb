1030128404f4b397343fdb432a4efe9b4903f146
Ginger Griffis <gingergriffis@hotmail.com>
2013-07-28 12:31:14 -0700

    Added edit user functionality and got specs working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 class UsersController < ApplicationController
+  before_filter :find_user, :only => [:show, :edit, :update, :destroy]
 
   def index
   end
 
   def show
-    @user = User.find(params[:id])
     @object = @user
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Profile has been updated."
+      redirect_to @user
+    else
+      flash[:alert] = "Profile has not been updated."
+      render :action => "edit"
+    end
+  end
+
+
+private
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
--- spec/integration/edit_user_spec.rb
+++ spec/integration/edit_user_spec.rb
@@ -2,10 +2,13 @@ require 'spec_helper'
 feature 'Editing user profile' do
 
   before do
-    Factory(:user, :email => "codefellow@example.com")
-    User.find_by_email('codefellow@example.com')
+    Factory(:user, :email => "codefellow@example.com", :first_name => "Ginger",
+                              :last_name => "Griffis")
     visit '/'
     click_link 'Sign in'
+    fill_in 'Email', :with => "codefellow@example.com"
+    fill_in 'Password', :with => "password"
+    click_button "Sign in"
     click_link "Edit Profile"
   end
 
@@ -13,8 +16,9 @@ feature 'Editing user profile' do
     fill_in 'First name', :with => "Thu"
     fill_in 'Last name', :with => "Quach"
     click_button "Update profile"
+    user = User.find_by_email('codefellow@example.com')
     page.should have_content("Profile has been updated.")
-    page.should have_content("Thu")
+    page.should_not have_content("Ginger")
     page.current_url.should == user_url(user)
   end
 
