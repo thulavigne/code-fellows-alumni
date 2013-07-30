7255ffcda5ec87543faa655146a7456b52031498
Ginger Griffis <gingergriffis@hotmail.com>
2013-07-30 13:31:35 -0700

    Added error messages to edit/create views

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,8 @@
 class Link < ActiveRecord::Base
   attr_accessible :description, :owner, :url_type, :url_text
   belongs_to :owner, :polymorphic => true
-  validates_format_of :url_text, :with => URI::regexp(%w(http https))
+  validates_format_of :url_text, :with => URI::regexp(%w(http https)),
+    :message => "must be the full URL including http::// or https:// so that it will be clickable."
 
   def self.url_types
     return [
