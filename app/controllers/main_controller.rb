80fd936f3258eb7ea018940d2412ad7261a8a296
Ginger Griffis <gingergriffis@hotmail.com>
2013-07-31 09:53:30 -0700

    Added homepage elements

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,8 @@
 class MainController < ApplicationController
   def index
     @users = User.all
+    if !@users.empty?
+      @random_user = @users.at(Random.rand(@users.size))
+    end
   end
 end
