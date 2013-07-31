9e2a68ebeb42ec5d1a53790aaa70924cbbbab688
Ginger Griffis <gingergriffis@hotmail.com>
2013-07-31 10:17:32 -0700

    Homepage refinement

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,10 @@ class MainController < ApplicationController
     @users = User.all
     if !@users.empty?
       @random_user = @users.at(Random.rand(@users.size))
+      @projects = @random_user.projects
+      if !@projects.empty?
+        @random_project = @projects.at(Random.rand(@projects.size))
+      end
     end
   end
 end
