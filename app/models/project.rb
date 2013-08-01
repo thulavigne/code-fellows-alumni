aa548f1c427745954fe787e4aedbad44d7c76d17
Thu Quach <thuquach90@gmail.com>
2013-07-31 19:39:02 -0700

    Made changes to homepage style.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ActiveRecord::Base
 
   belongs_to :user
   has_many :links, :as => :owner
-  has_attached_file :attach, :styles => { :medium => "200x200>", :small => "150x150>", :thumb => "100x100>" }, :default_url => "http://pickaface.net/avatar/ppic.jpg"
+  has_attached_file :attach, :default_url => "http://pickaface.net/avatar/ppic.jpg"
 
   validates_presence_of :title
 
