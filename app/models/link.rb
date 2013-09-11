35b2e8beffd2703dc5750cac1d7d2b9077ba802a
Ginger Griffis <gingergriffis@hotmail.com>
2013-09-11 16:35:03 -0700

    Fixed link error message

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@ class Link < ActiveRecord::Base
   attr_accessible :description, :owner, :url_type, :url_text
   belongs_to :owner, :polymorphic => true
   validates_format_of :url_text, :with => URI::regexp(%w(http https)),
-    :message => "must be the full URL including http::// or https:// so that it will be clickable."
+    :message => "must be the full URL including http:// or https:// so that it will be clickable."
   validates_presence_of :url_text
 
   def home_page?
